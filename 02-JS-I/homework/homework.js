dbc41d7f37293b2d96504ca31dfe8a862f68f05f
basto <eirow@DESKTOP-JB6IDAU>
2022-09-26 17:09:50 +0900

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 6;
+const nuevoNum = 62;
 
 // Crea una variable booleana:
 const nuevoBool = null;
